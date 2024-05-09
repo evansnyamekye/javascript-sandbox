a527e54f9971afd27fef5c604203c3a77f694088
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-05-09 19:37:24 +0000

    update: add variables to feature/variables

---
+++ 03-variables/script.js
@@ -0,0 +1,30 @@
+let firstname = 'kofi';
+let lastname = 'acquah';
+
+console.log(firstname, lastname)
+
+
+let score; 
+
+score = 1; 
+
+console.log(score);
+if (true){
+    score = score + 2; 
+} 
+console.log(score); 
+
+const arr = [1,2,3,4,5]; 
+
+arr.push(6)
+
+console.log(arr); 
+
+const person = {
+    name: 'derby'
+};
+
+person.name = 'kwame'
+person.email = 'acquah@hotmail.com'
+
+console.log(person);
