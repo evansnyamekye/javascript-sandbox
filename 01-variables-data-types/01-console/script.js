b664a3e32a26256c597571b72c72033022d46d69
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-05-09 06:48:11 +0000

    first commit

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,21 @@
+//log number 
+console.log(100);
+
+//log string 
+console.log('Hello Kofi');
+
+//log multiple values
+console.log('Amount is', 980, 'which is', true );
+
+//log a variable 
+const x = 100;
+console.log(x); 
+
+//log error as table 
+console.error('alert');
+console.warn('warning'); 
+
+// log object as table 
+console.table({ name: 'evans', email: 'nyamekye2131@gmail.com'})
+console.table({ fname: 'Evans', lname: 'Smith', email: 'acquah101@hotmail.com', phone: '233 42547856'})
+
