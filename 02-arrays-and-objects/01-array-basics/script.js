a6e8853aa0e60d8f77b67a7393a41b28f8935bbb
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-05-11 22:00:49 +0000

    add arrays folder

---
+++ 02-arrays-and-objects/01-array-basics/script.js
@@ -0,0 +1,15 @@
+let x;
+const numbers = [12,45,2,5,3];
+console.log(numbers); 
+x = numbers[2];
+const fruist = new Array('apple', 'mango', 'orange')
+console.log(fruist);
+
+x = numbers[0] + numbers[2]; 
+
+x = `My favorite fruit is ${fruist[1]}`; 
+
+const num = new Array(78,85,5,6); 
+console.log(num);
+
+console.log(x)
