813ade37afee9c0a425b1458a3eb024693b7d6af
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-05-15 13:12:36 +0000

    Update: object challenge

--- 02-arrays-and-objects/09-object-challenge/script.js
+++ 02-arrays-and-objects/09-object-challenge/script.js
@@ -1,46 +1,45 @@
-//step 1
+// Step 1
 const library = [
-    {
-    title: 'the god father',
-    author: 'scarface',
+  {
+    title: 'The Road Ahead',
+    author: 'Bill Gates',
     status: {
-        own: true,
-        reading: false, 
-        read: false
+      own: true,
+      reading: false,
+      read: false,
     },
-},
-    {
-    title: 'Windows',
-    author: 'bill gate',
+  },
+  {
+    title: 'Steve Jobs',
+    author: 'Walter Isaacson',
     status: {
-        own: true,
-        reading: false, 
-        read: false
+      own: true,
+      reading: false,
+      read: false,
     },
-},
-    {
-    title: 'escalete-Inherittance',
-    author: 'bond will',
+  },
+  {
+    title: 'Mockingjay',
+    author: 'Suzanne Collins',
     status: {
-        own: true,
-        reading: false, 
-        read: false
+      own: true,
+      reading: false,
+      read: false,
     },
-}
-]; 
+  },
+];
 
-//step 2
-library[0].status.reading = true;
-library[1].status.reading = true;
-library[2].status.reading = true;
+// Step 2
+library[0].status.read = true;
+library[1].status.read = true;
+library[2].status.read = true;
 
-//step 3
-const { title: firstbook } = library[0]; 
-console.log(firstbook)
+// Step 3
+const { title: firstBook } = library[0];
 
+console.log(firstBook);
 
+// Step 4
+const libraryJSON = JSON.stringify(library);
 
-
-
-
-
+console.log(libraryJSON);
