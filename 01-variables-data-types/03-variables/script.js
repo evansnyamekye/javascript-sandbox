e9a36de0a5eac8b6c5fc5bcad4fe6927b1e9c0fd
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-05-27 12:40:12 +0000

    update: modify all functions files in complete array__folder

--- 01-variables-data-types/03-variables/script.js
+++ 01-variables-data-types/03-variables/script.js
@@ -1,30 +1,60 @@
-let firstname = 'kofi';
-let lastname = 'acquah';
+// Ways to declare a variable
+// `var`, `let`, & `const`
 
-console.log(firstname, lastname)
+let firstName = 'John';
+const lastName = 'Doe';
+let age = 30;
 
+console.log(age);
 
-let score; 
+// Naming Conventions
+// - Only letters, numbers, underscores and dollar signs
+// - Can't start with a number
 
-score = 1; 
+// Multi-Word Formatting
+// firstName = camelCase
+// first_name = underscore
+// FirstName = PascalCase
+// firstname = lowercase
+
+// We can re-assign `let` variables. If you change `age` to use `const`, you will get an error
+age = 31;
+console.log(age);
+
+// With let, we can declare a value without assigning a value
+let score;
+score = 1;
+console.log(score);
+
+if (true) {
+  score = score + 1;
+}
 
 console.log(score);
-if (true){
-    score = score + 2; 
-} 
-console.log(score); 
 
-const arr = [1,2,3,4,5]; 
+const x = 100;
 
-arr.push(6)
+// We can not re-assign a const variable
+// x = 200 // Will result in an error
 
-console.log(arr); 
+// We can still manipulate arrays and objects using const
+const arr = [1, 2, 3, 4];
+arr.push(5);
+console.log(arr);
 
 const person = {
-    name: 'derby'
+  name: 'Brad',
 };
-
-person.name = 'kwame'
-person.email = 'acquah@hotmail.com'
-
+person.name = 'John';
+person.email = 'brad@gmail.com';
 console.log(person);
+
+// Declare multiple values at once
+let a, b, c;
+
+const d = 10,
+  e = 20,
+  f = 30;
+
+console.log(d);
+console.log(a);
