4953e6c9388f199500c7b59b3f3c7936097c1ca3
Evans Kofi Nyamekye <nyamekye2131@gmail.com>
2024-05-27 12:33:35 +0000

    commit

--- 05-Loops, Iteration & array methods/11-array-method-challenges/quiz.js
+++ 05-Loops, Iteration & array methods/11-array-method-challenges/quiz.js
@@ -45,3 +45,60 @@ const people = [
 const youngPeople = people.filter((person) => person <= 25); 
 
 console.log(youngPeople);
+
+/*
+1. From the products array.
+2. create an array called availableProducts that contains objects with ONLY 
+ the productName and price properties of products 
+ that are in stock.
+3. The productName property should 
+ be a concatenation of the brand and model.
+*/
+const products = [
+  {
+    productName: 'spagatti',
+    brand: 'sicilly-wave',
+    model: 'FDF-Q23',
+    shopLinStock: true,
+    price: 48
+  },
+  {
+    productName: 'rice',
+    brand: 'tatalia-longrain',
+    model: 'THIA-R21',
+    inStock: true,
+    price: 211
+  },
+  {
+    productName: 'beef',
+    brand: 'excerter',
+    model: 'EV231',
+    inStock: true,
+    price: 241
+  },
+  {
+    productName: 'chicken',
+    brand: 'Darko-farms',
+    model: 'DKR-90',
+    inStock: true,
+    price: 643
+  },
+  {
+    productName: 'cheese',
+    brand: 'barely-BRAD',
+    model: 'ChessKits',
+    shopLocinStock: true,
+    price: 48
+  }
+];
+
+const availableProducts = products.filter(product => 
+product.inStock)
+  .map((product) => ({
+   productName: `${product.brand}`,
+   price: `${product.price}`                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+  })
+ );
+
+
+console.log(availableProducts)
