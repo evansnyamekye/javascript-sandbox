2039d4cd5b2efc36bef47568f899784ff5c95eb8
Evans K. Nyamekye <nyamekye2131@gmail.com>
2024-05-29 22:18:06 +0000

    new arti

--- 05-Loops, Iteration & array methods/11-array-method-challenges/quiz.js
+++ 05-Loops, Iteration & array methods/11-array-method-challenges/quiz.js
@@ -42,6 +42,67 @@ const people = [
     },
   ];
 
-const youngPeople = people.filter((person) => person <= 25); 
+  /*
+ 1. Take the people array 
+ 2. Create an array called youngPeople - stores objects with ONLY name & email.
+ 4. properties of all the people that are 25 and under
+ 3.the `name` properties should have first name and last name 
+  */
 
-console.log(youngPeople);
+const youngPeople = people.
+filter((person) => person.age <= 25)
+.map((person) => ({
+    email: person.email, 
+    phone: person.phone,
+    name: person.firstName + ' ' + person.lastName,
+}))
+console.log(youngPeople); 
+
+/*
+Take the employees array and create an array called experiencedEmployees that 
+stores objects with ONLY the name and position properties of all the employees
+ that have more than 5 years of experience. The name property should be a combination of first name and last name.
+*/
+
+const employees = [
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@gmail.com',
+      experience: 15,
+    },
+    {
+      firstName: 'Jane',
+      lastName: 'Poe',
+      email: 'jane@gmail.com',
+      experience: 47,
+    },
+    {
+      firstName: 'Bob',
+      lastName: 'Foe',
+      email: 'bob@gmail.com',
+      experience: 25,
+    },
+    {
+      firstName: 'Sara',
+      lastName: 'Soe',
+      email: 'Sara@gmail.com',
+      experience: 2
+    },
+    {
+      firstName: 'Jose',
+      lastName: 'Koe',
+      email: 'jose@gmail.com',
+      phone: '555-555-5555',
+      experience: 2
+    },
+  ];
+
+const experiencedEmployees = employees.
+filter((person) => person.experience >= 5)
+.map((employees) => ({
+    name: employees.firstName + ' ' + employees.lastName,
+    email: employees.email
+}))
+
+console.log(experiencedEmployees); 
