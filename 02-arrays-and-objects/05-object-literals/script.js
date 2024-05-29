9217a4f2703caef0561124ce2c240ccb13e33c9c
Evans kofi Nyamekye <nyamekye2131@gmail.com>
2024-05-29 21:48:05 +0000

    Revert "Add fuunctionalities to GTA"

--- 02-arrays-and-objects/05-object-literals/script.js
+++ 02-arrays-and-objects/05-object-literals/script.js
@@ -1,47 +1,24 @@
-let x;
-
-// Creating an object
+let x; 
 const person = {
-  name: 'John Doe',
-  age: 30,
-  isAdmin: true,
-  address: {
-    street: '123 Main st',
-    city: 'Boston',
-    state: 'MA',
-  },
-  hobbies: ['music', 'sports'],
-};
+        name: 'Evans K', 
+        age: '38', 
+        isAdmin: true,
+        address: {
+            street: '#7 new bortianor - 2nd street Golden',
+            state: 'Greater Accra - Ga South', 
+            city: 'West Hills - Weija'
+        },
 
-// Accessing object properties
-x = person.name; // Dot notation
-x = person['age']; // Bracket notation
-x = person.address.state;
-x = person.hobbies[0];
+        hobbies: 'music, dancing'
+    }; 
 
-// Updating properties
-person.name = 'Jane Doe';
-person['isAdmin'] = false;
+    x = person.name; 
+    x = person.address.state; 
+    x = person.hobbies;
 
-// Deleting properties
-delete person.age;
+    person.name = 'Auntie Ceci';
 
-// Create new properties
-person.hasChildren = true;
-
-// Add functions
-person.greet = function () {
-  console.log(`Hello, my name is ${this.name}`);
-};
-
-person.greet();
-
-// Keys with multiple words
-const person2 = {
-  'first name': 'Brad',
-  'last name': 'Traversy',
-};
-
-x = person2['first name'];
+    x = person; 
 
 console.log(x);
+
