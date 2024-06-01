b8ed0260278b19bb5e7150a41afad23e5f34e48f
Evans K. Nyamekye <nyamekye2131@gmail.com>
2024-06-01 03:28:50 +0000

    commit files

--- 06-document-object-model/07-create-elements/shopping-list/script.js
+++ 06-document-object-model/07-create-elements/shopping-list/script.js
@@ -1,13 +1,6 @@
-const div = document.createElement('div');
-div.className = 'my-element';
-div.id = 'my-element';
-div.setAttribute('title', 'My Element');
+const div = document.createElement('div')
+const text = document.createTextNode('Hello Nested')
 
-// div.innerText = 'Hello World';
+div.appendChild(text); 
 
-const text = document.createTextNode('Hello World');
-div.appendChild(text);
-
-// document.body.appendChild(div);
-
-document.querySelector('ul').appendChild(div);
+document.body.appendChild(div); 
