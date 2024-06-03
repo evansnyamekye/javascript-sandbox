5bc13543afef1796af73fa73acb6a23ad763cb8b
Evans kofi Nyamekye <nyamekye2131@gmail.com>
2024-06-03 23:22:36 +0000

    Revert "Feature/array cha"

--- 06-document-object-model/05-traversing-the-dom-elements/script.js
+++
@@ -1,8 +0,0 @@
-const parent = document.querySelector('.parent')
-const child = document.querySelector('.child')
-
-document.body.style.color = "red"; 
-document.body.style = "font-weight: 700"  
-
-console.log(parent); 
-console.log(child); 
--- 06-document-object-model/07-create-elements/shopping-list/script.js
+++
@@ -1,6 +0,0 @@
-const div = document.createElement('div')
-const text = document.createTextNode('Hello Nested')
-
-div.appendChild(text); 
-
-document.body.appendChild(div); 
