1570fbb0aaf01acf8e67df1a09169f951979eaec
Evans kofi Nyamekye <nyamekye2131@gmail.com>
2024-06-05 21:48:29 +0000

    Revert "Revert "Add fuunctionalities to GTA""

--- 01-variables-data-types/12-math-object/script.js
+++ 01-variables-data-types/12-math-object/script.js
@@ -1,31 +1,33 @@
+let x;
 
-const x = Math.floor(Math.random() * 100 + 1); 
-const y = Math.floor(Math.random() * 50 + 1);
+// Square root
+x = Math.sqrt(9);
 
-console.log(x, y); 
+// Absolute value
+x = Math.abs(-5);
 
-//Get the sum
-const sum = x + y; 
-const sumOutPut =  `${x} + ${y}; = ${sum}`
-console.log(sumOutPut); 
+// Round
+x = Math.round(4.2);
 
-//Get the different
-const diff = x - y; 
-const diffOutPut =  `${x} - ${y}; = ${diff}`
-console.log(diffOutPut); 
+// Round up
+x = Math.ceil(4.2);
 
-//Get the product
-const prod = x * y; 
-const productOutPut =  `${x} * ${y}; = ${prod}`
-console.log(productOutPut); 
+// Round down
+x = Math.floor(4.9);
 
-//Get the quotient
-const qua = x / y; 
-const quodientOutPut =  `${x} / ${y}; = ${qua}`
-console.log(quodientOutPut); 
+// Exponent
+x = Math.pow(2, 3);
 
+// Minimum number
+x = Math.min(4, 5, 3);
 
-//Get the sum
-const reminder = x % y; 
-const reminderOutPut =  `${x} % ${y}; = ${reminder}`
-console.log(reminderOutPut); 
+// Maximum number
+x = Math.max(4, 5, 3);
+
+// Get a random number/decimal between 0 and 1
+x = Math.random();
+
+// Get a random number between 1 and 100
+x = Math.floor(Math.random() * 100 + 1);
+
+console.log(x);
